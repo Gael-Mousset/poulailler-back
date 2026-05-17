8826ffe5cf8da37bda490e388512f22d8f719b86
Gael Mousset <gaelmousset@outlook.fr>
2026-05-17 23:06:39 +0200

    ready for deploy

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:8085'],
+    origin: [
+      process.env.FRONTEND_URL ?? 'http://localhost:8085',
+      'http://localhost:5173',
+    ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type'],
   });
